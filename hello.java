118892be2c6dd9a1b5f9c16198d8ea3e72576666
linus-secondary <edricplinus@gmail.com>
2024-11-27 18:21:19 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 public class hello{
     public static void main (String[] args){
         System.out.println("Hello world!! anytime at your service");
+        System.out.println("iam working on jenkins and git");
     }
 }
